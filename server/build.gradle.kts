02e6a71833286e27f9bb54f1561352935db4f4e5
Riccardo Gracis <riccardo.gracis@gmail.com>
2022-03-22 14:04:57 +0100

    Feat: add simple test case and removed JPA DB dependencies in the project

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -24,16 +24,14 @@ repositories {
 
 dependencies {
     implementation("com.auth0:java-jwt:3.19.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation ("mysql:mysql-connector-java:8.0.28")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    //implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //implementation ("mysql:mysql-connector-java:8.0.28")
+    //implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-web-services")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- server/src/test/kotlin/com/example/server/ServerApplicationTests.kt
+++ server/src/test/kotlin/com/example/server/ServerApplicationTests.kt
@@ -1,13 +1,36 @@
 package com.example.server
 
+import com.example.server.resources.GENERATE_TICKET
+import com.example.server.resources.ROOT_API
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ServerApplicationTests {
 
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+
     @Test
-    fun contextLoads() {
+    fun generateTicketTest() {
+        val value =  mvc.perform(MockMvcRequestBuilders.get(ROOT_API + GENERATE_TICKET)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andReturn()
+        val json = value.response.contentAsString
+        assertNotNull(json)
+        assertTrue(json.isNotEmpty())
     }
 
 }
